f4fd3e9f0e3023d154cfd65adf38f3f107d87f5a
Isa <isabel.ciartano@gmail.com>
2025-11-21 12:41:50 +0100

    Update dizionario2.py

--- compiti/dizionario2.py
+++ compiti/dizionario2.py
@@ -1,8 +1,10 @@
 import uuid
+
 def getMyMac():
     mac=uuid.getnode()
     mac_str=":".join(f"{(mac>>i)&0xff:02x}"for i in range (40,-1-8))
     return mac_str
+
 def preparaMac(mac_str):
     mac_str=mac_str.replace("-",":")
     mac_str.upper()
@@ -11,9 +13,7 @@ def preparaMac(mac_str):
 def main():
     file=open("./mac-vendors-export.csv","r",encoding ='utf-8')
     righe=file.readlines()#righe è una lista di stringhe 
-
     file.close
-
     elenco={}
     mac=[]
     vendors=[]
@@ -21,7 +21,6 @@ def main():
         campi=riga.split(",")#campi è una lista di stringhe 
         mac.append(campi[0])
         vendors.append(campi[1])
-
     for m,v in zip(mac,vendors):
         chiave=m.upper()
         valore=v
@@ -32,6 +31,7 @@ def main():
         print(elenco[ricerca])
     else:
         print("mac non trovato")
+         
 if __name__ == "__main__":
     main()
 
