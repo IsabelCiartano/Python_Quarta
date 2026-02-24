a65670ca10396c1ed0f9623fffc495366c064d3c
Isa <isabel.ciartano@gmail.com>
2026-02-24 15:03:20 +0100

    Update social_network.py

--- social_network.py
+++ social_network.py
@@ -1,7 +1,11 @@
 #definizione social network una rete in cui gli utenti si connettono
 
 class Rete():
-    pass
+    def __init__(self):
+        self.utenti=[]
+    def iscrivi_utente(self,utente):#utente è un oggetto di tipo Utente
+        self.utenti.append(utente)
+    
 class Utente():
     def __init__(self,nome):
         self.nome=nome
@@ -9,4 +13,19 @@ class Utente():
     def aggiungi_amico(self,utente):
         pass
     def __str__(self):
-        #stampa utente e i sui contatti 
+        #stampa utente e i sui contatti(=amici)
+        pass
+
+def main():
+    social_network=Rete()
+    luca=Utente("luca")
+    andrea=Utente("andrea")
+    mario=Utente("mario")
+
+    social_network.iscrivi_utente(luca)
+    social_network.iscrivi_utente(andrea)
+    social_network.iscrivi_utente(mario)
+
+if __name__=="__main__":
+    main()
+    
